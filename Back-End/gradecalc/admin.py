947cbb631e6ba746d68b88f11f8b4561079a1323
Goodxyza <kkharnboriwarnk@gmail.com>
2025-10-31 21:06:10 +0700

    Move function from admin to model

--- Back-End/gradecalc/admin.py
+++ Back-End/gradecalc/admin.py
@@ -8,7 +8,3 @@ from .models import GradeResult
 class GradeResultAdmin(admin.ModelAdmin):
     list_display = ("id", "total_gpa", "grade_letter", "created_at")
     ordering = ("-created_at",)
-
-
-def __str__(self):
-    return f"GPA {self.total_gpa:.2f} ({self.grade_letter}) - {self.owner.username if self.owner else 'Guest'}"
