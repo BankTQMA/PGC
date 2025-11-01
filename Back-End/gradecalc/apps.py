d3b75dcfeb43a1aaccf309738f3d688fbe0f892c
Therdsak Chen <minecraftforce1234@gmail.com>
2025-11-01 11:36:50 +0700

    Install Black (Pipfile updated) and format all Python files with it

--- Back-End/gradecalc/apps.py
+++ Back-End/gradecalc/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GradecalcConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gradecalc'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "gradecalc"
